b472bc8c956a7156ecd34e368b850f0bfc20f59e
rocalderon <rocalderon@CLDFC02FC1FFMD6N.local>
2024-11-06 16:44:11 -0300

    siguiente commit

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/raymoncalderon/godesde0/variables"
 )
 
 func main() {
-	variables.MuestroEnteros()
+	estado, texto := variables.ConviertoaTexto(1588)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
